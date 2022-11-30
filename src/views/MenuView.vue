552da26753d22124429906ff6add17dbc0a034ff
Liam Battini <liamb@kth.se>
2022-11-30 10:46:50 +0100

    made it easier to test the views

---
+++ src/views/MenuView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink} from 'vue-router'
+</script>
+
+<template>
+  <div class="nav">
+    <p>CryptoViewer</p>
+    <RouterLink to="/MostPopularCrypto">Home</RouterLink>
+    <RouterLink to="/UserSearchForm">Find Users</RouterLink>
+    <RouterLink to="/SignInForm">Sign in</RouterLink>
+    <RouterLink to="/SignUpForm">Sign up</RouterLink>
+  </div>
+</template>
+
+<style>
+.nav {
+  background-color: #26c545;
+}
+.nav a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+</style>
