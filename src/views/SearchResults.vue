1d9372e8c7030ea2ab2b7724e859121b773d44d4
ingem <ingemarc@kth.se>
2022-12-07 15:03:31 +0100

    small fix

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,7 +1,5 @@
-
-
 <script>
-
+import { RouterLink} from 'vue-router'
 export default {
   name: "SearchResults"
 }
@@ -9,16 +7,31 @@ export default {
 
 <template>
   <div class="search-wrapper">
-    <label for="search">Search Users</label>
+    <label for="search" id="search"></label>
     <input type="search" id="search">
   </div>
-  <div class="user-cards" data-user-cards-container></div>
-  <template data-user-template>
-    <div class="card">
-    <div class="header" data-header></div>
-    <div class="body" data-body></div>
+  <div class="user-cryptos">
+    <div class="crypto">
+      <div class="header">CryptoName</div>
+      <div class="body">Other</div>
+    </div>
+    <div class="crypto">
+      <div class="header">CryptoName</div>
+      <div class="body">Other</div>
+    </div>
+    <div class="crypto">
+      <div class="header">CryptoName</div>
+      <div class="body">Other</div>
+    </div>
+    <div class="crypto">
+      <div class="header">CryptoName</div>
+      <div class="body">Other</div>
+    </div>
+    <div class="crypto">
+      <div class="header">CryptoName</div>
+      <div class="body">Other</div>
+    </div>
   </div>
-  </template>
 </template>
 
 <style>
@@ -33,40 +46,32 @@ export default {
   text-decoration: none;
   font-size: 17px;
 }
-
 .search-wrapper {
   display: flex;
   flex-direction: column;
   gap: .25rem;
-
 }
-
 input{
   font-size: 1rem;
 }
-
-.user-cards{
+.user-cryptos{
   display: grid;
   grid-template-columns: 1fr;
   gap: .25rem;
   margin-top: 1rem;
 }
-
-.card{
+.crypto{
   border: 1px solid black;
   background-color: grey;
   padding: .5rem;
 }
-
 .card > .header{
   margin-bottom: .25rem;
 }
-
 .card > .body {
   font-size: .8rem;
   color: black;
 }
-
 .hide{
   display: none;
 }
