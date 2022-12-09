c14c4315aedb79bed55a2f963ed0d7f2a8d279bb
ingem <ingemarc@kth.se>
2022-12-09 16:35:59 +0100

    added user information page

---
+++ src/views/UserInformationView.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="user-information">
+<h1>FirstName LastName</h1>
+  <h2>Description</h2>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "UserInformationView"
+}
+</script>
+
+<style scoped>
+
+</style>
