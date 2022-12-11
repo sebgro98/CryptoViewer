a27880c83a56ff7535c68f76bbb6f4e727c5ac3e
Liam Battini <liamb@kth.se>
2022-12-11 17:27:22 +0100

    Search works now in home

--- src/cryptoSource.js
+++ src/cryptoSource.js
@@ -1,27 +1,16 @@
 import axios from "axios";
-import {ref, watchEffect} from "vue";
 
+function searchCryptos(endpoint) {
+    return axios.get('https://api.coingecko.com/api/v3/search?query='+endpoint).then(treatHTTPResponseACB)
+}
 
-let cryptos = ref([])
-let clone = ref([])
-//api call
-const getCrypto = async () => {
-    try {
-        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-        cryptos.value = response.data
-    } catch (error) {
-        //console.log(error);
+function treatHTTPResponseACB(response) {
+    if (response.status !== 200) {
+        throw new Error("API Response Time: " + response.status);
     }
-};
-//fetch timer do not overextend our limit
-setInterval(() => {
-    getCrypto();
-}, 8000);
+    else {
+        return response.data;
+    }
+}
 
-watchEffect(() => {
-    const dup = cryptos.value.slice(0,100);
-    clone.value = dup;
-})
-
-
-export {clone}
+export {searchCryptos}
