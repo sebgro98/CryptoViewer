384edeb94a5162033c14300b5d491e0991023857
Sushil's pc <sushilkc855@outlook.com>
2022-12-19 17:26:37 +0100

    .

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", redirect: "/home" },
+    { path: "/", redirect: "/login" },
     {
       path: '/home',
       name: 'home',
